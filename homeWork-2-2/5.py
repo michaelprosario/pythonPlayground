b1ada40ed5005226e714b87d5076e23afdf3db0f
michaelprosario <michael.p.rosario@gmail.com>
2024-02-05 01:06:27 +0000

    feat ( peter hw ) wip

--- homeWork-2-2/5.py
+++ homeWork-2-2/5.py
@@ -14,7 +14,7 @@ def get_index():
         #if index is greater than the available indexs its not valid
         #else continue becausr index not valid
 
-        if index > -1 and index < len(word):
+        if index > -2 and index < len(word):
             return index
         else:
             print ("invalid index, input valid index: ")
@@ -31,7 +31,7 @@ def get_letter():
     #while loop asking for an acceptabe amount of characters
     while needToLoop:
         #use input to get the character
-        out = input("enter valid : ")
+        out = input("enter valid letter: ")
     
         if len(out) == 1:
             return out
@@ -40,15 +40,17 @@ def get_letter():
 
 
 # setup list to represent the word
-
+wordAsList = list(word)
 while True:
     # get index to change
     mainIndex = get_index()
+    if mainIndex ==-1:
+        break
+
     # get the letter
+    letter = get_letter()
 
     # based on the inputs change the word list please
-
+    wordAsList[mainIndex] = letter
     # After the change, write out word list to the screen so user can see updates
-
-k = get_letter();
-print(k)
+    print ("".join(wordAsList))
